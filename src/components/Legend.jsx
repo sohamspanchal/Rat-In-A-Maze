86ac31f6497e1c72e85ba804a459ac5d7aff6baa
soham <116659050+sohamspanchal@users.noreply.github.com>
2024-07-30 23:26:34 +0530

    Update Legend.jsx

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -49,7 +49,7 @@ export default function Legend() {
 					This algorithm is built in way that the rat can only make new moves in
 					only 'down' and 'right' directions.{' '}
 					<a
-						href="https://github.com/prtvi/rat-in-a-maze"
+						href="https://github.com/sohamspanchal/Rat-In-A-Maze.git"
 						target="_blank"
 						rel="noreferrer"
 					>
